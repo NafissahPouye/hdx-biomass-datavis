a148f3aa21fa2af80a500c1f1be6908703f68bac
NafissahPouye <nafissatou.pouye@aims-senegal.org>
2017-05-05 12:52:17 +0000

    Fix the choropleth and adjust the biomass linechart

--- js/site.js
+++ js/site.js
@@ -11,52 +11,54 @@ function generateringComponent(vardata, vargeodata){
   var chartDimension = cf.dimension(function(d) { return d.year}) ;
   var mapDimension = cf.dimension(function(d) { return d.rowcacode2});
 
-  var chartGroup = chartDimension.group().reduceSum(function(d){ return d.biomass});
+  var chartGroup = chartDimension.group().reduceSum(function(d){ return d.biomass/1000});
   var mapGroup = mapDimension.group().reduceSum(function(d){ return d.anomalie});
 
-  biomass_chart.width(475)
-               .height(450)
+  biomass_chart.width(550)
+               .height(500)
                .dimension(chartDimension)
                .group(chartGroup)
                .x(d3.scale.linear().domain([1998, 2016]))
                .renderArea(true)
-               .margins({top: 20, right: 0, bottom: 30, left: 90})
+               .margins({top: 10, right: 13, bottom: 80, left: 80})
                .renderHorizontalGridLines(true)
                .renderVerticalGridLines(true)
                .elasticY(true)
                .colors('#03a9f4')
-               .colorAccessor(function(d,i){ return 0;});
-               //.xAxis().ticks(5);
+               .colorAccessor(function(d,i){ return 0;})
+              .renderlet(function (chart) {
+                    chart.selectAll("g.x text")
+                      .attr('dx', '-12')
+                      .attr('transform', "rotate(-60)");
+                })
+               .xAxis().tickFormat(d3.format("d"));
+               //.yAxis().tickFormat(d3.format("d"));
+
 
 dc.dataCount('#count-info')
   .dimension(cf)
   .group(all);
 
 
- anomalychoroplethmap.width(450)
-             .height(450)
+ anomalychoroplethmap.width($('#anomalyMap'))
+             .height(500)
              .dimension(mapDimension)
              .group(mapGroup)
              .center([0,0])
              .zoom(0)
              .geojson(vargeodata)
-             .colors(['#0080FF','#FE9A2E','#FACC2E','#71A5CA', '#0B6121','#0B3B0B'])
-             .colorDomain([0,6])
+             .colors(['#CEF6CE','#F5DA81', '#58FAAC', '#01DF3A'])
+             //.colors(['#334605','#DDDDDD','#C59512','#EFB412','#B2F013' ,'#DDDDDD'])
+             .colorDomain([0,3])
              .colorAccessor(function (d){
                var c =0
-                if(d<75){
+                if(d<110){
                     c=  1;
-                } else if (d>=75 & d<90) {
+                } else if (d>=110& d<150) {
                     c=2;
-                } else if (d>=90 & d<110) {
+                } else if (d>=150) {
                     c=3;
-                } else if (d>=110 & d<150) {
-                    c=4;
-                } else if (d>=150 & d<175) {
-                    c=5;
-                } else if (d>175) {
-                    c=6;
-                };
+                } 
                 return c
                 
             })         
